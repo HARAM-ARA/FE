215c2e59de5ea811fd2dcd314826085391ee14ca
yoonmisu <ymisu9@gmail.com>
2025-12-01 03:04:46 +0900

    chore : 어드민 홈 화면 퍼블리싱

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -123,7 +123,7 @@ export default function Header({teamName, isTeamName = false, isLogin = false, i
       <Headerbox>
         <LogoImg src={HaramLogo}></LogoImg>
         <FunctionBox>
-            {isTeamName && <AmountText>TEAM {teamName}</AmountText>}
+            {isTeamName && <AmountText>{teamName}</AmountText>}
          
             {isLogin && <LoginBtn type="google" onClick={handleGoogleLogin}>로그인</LoginBtn>}
             {isCredit && <CreditBtn><CreditColor>{Credit}</CreditColor><Gray>크레딧</Gray></CreditBtn>}
